4e9c0a2db2cdec350987bb9be639f478cf2ff7f7
Cutecookies <cat.O1204@yandex.ru>
2025-06-02 15:12:34 +0300

    First commit

---
+++ time.cpp
@@ -0,0 +1,44 @@
+#include "time.h"
+
+Time::Time(int h, int m) : hours(h), minutes(m) {
+    validate();
+}
+
+void Time::validate() {
+    if (hours < 0) {
+        hours = 0;
+    } else if (hours > 23) {
+        hours = 23;
+    }
+
+    if (minutes < 0) {
+        minutes = 0;
+    } else if (minutes > 59) {
+        minutes = 59;
+    }
+}
+
+void Time::addMinutes(int min) {
+    minutes += min;
+    hours += minutes / 60;
+    minutes %= 60;
+    hours %= 24;
+    validate();
+}
+
+int Time::toMinutes() const {
+    return hours * 60 + minutes;
+}
+
+std::string Time::toString() const {
+    return (hours < 10 ? "0" : "") + std::to_string(hours) + ":" +
+           (minutes < 10 ? "0" : "") + std::to_string(minutes);
+}
+
+int Time::getHours() const {
+    return hours;
+}
+
+int Time::getMinutes() const {
+    return minutes;
+}
---
+++ time.h
@@ -0,0 +1,21 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <string>
+
+class Time {
+private:
+    int hours;
+    int minutes;
+
+public:
+    Time(int h = 0, int m = 0);
+    void validate();
+    void addMinutes(int min);
+    int toMinutes() const;
+    std::string toString() const;
+    int getHours() const;
+    int getMinutes() const;
+};
+
+#endif // TIME_H
